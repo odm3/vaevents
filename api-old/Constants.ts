df611588ddb27203c42f20a647fa9f268ba8e536
Oscar D McCullough <odm3productions@gmail.com>
2022-07-02 14:32:41 -0400

    init

---
+++ api-old/Constants.ts
@@ -0,0 +1,8 @@
+export const _CONSTANTS_ = {
+    KEYVAULT_URL: `https://robotics-kv.vault.azure.net`,
+    SECRET_NAME: `roboteventsAuth`,
+    ROBOTEVENTSURL: `https://www.robotevents.com/api/v2/seasons`,
+    SEASON_IDS: [173, 174],
+    REGION: "Virginia",
+    PAGE_LIMIT: 100
+}
